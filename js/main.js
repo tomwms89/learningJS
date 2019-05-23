5d8c893a8c800562ffd29a6be447f2acae3868f1
Wellington Macedo <wellington.macedo89@outlook.com>
2019-05-23 11:08:20 -0300

    Alteração do body

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ function soma(a, b) {
     return a + b;
 }
 
+document.getElementsByClassName ('title').innerHTML = "Isso é um teste";
+
 var resultado = soma(4, 9);
 resultado = resultado - 1;
 console.log("A soma é: ", resultado);
