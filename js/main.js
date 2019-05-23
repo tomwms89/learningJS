5bf73192373a050bba41e3405c666ab67865081d
Wellington Macedo <wellington.macedo89@outlook.com>
2019-05-23 15:34:09 -0300

    Novos testes JS

--- js/main.js
+++ js/main.js
@@ -10,6 +10,15 @@ function soma(a, b) {
 
 document.getElementsByClassName ('title').innerHTML = "Isso é um teste";
 
+var para = document.getElementById('testeMDN');
+
+para.addEventListener('click', atualizarNome);
+
+function atualizarNome() {
+  var nome = prompt('Insira um novo nome');
+  para.textContent = 'Jogador 1: ' + nome;
+}
+
 var resultado = soma(4, 9);
 resultado = resultado - 1;
 console.log("A soma é: ", resultado);
