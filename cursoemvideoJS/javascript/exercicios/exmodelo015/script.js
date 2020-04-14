097afdfa3f0ea66ede488c1966a0754ce0916287
Tom <wellington.macedo89@outlook.com>
2020-04-14 12:18:32 -0300

    segundo exercicio

--- cursoemvideoJS/javascript/exercicios/exmodelo015/script.js
+++ cursoemvideoJS/javascript/exercicios/exmodelo015/script.js
@@ -15,26 +15,35 @@ function verificar() {
             gênero = `Homem`
             if(formIdade >=0 && formIdade < 10) {
                 // criança
+                img.setAttribute(`src`, `foto-menino.png`)
             } else if (formIdade < 21) {
                 // jovem
+                img.setAttribute(`src`, `foto-jovem-h.png`)
             } else if (formIdade < 50) {
                 // adulto
+                img.setAttribute(`src`, `foto-adulto-h.png`)
             } else {
                 // idoso
+                img.setAttribute(`src`, `foto-idoso.png`)
             }
         } else if (formSex[1].checked) {
             gênero = `Mulher`
             if(formIdade >=0 && formIdade < 10) {
                 // criança
+                img.setAttribute(`src`, `foto-menina.png`)
             } else if (formIdade < 21) {
                 // jovem
+                img.setAttribute(`src`, `foto-jovem-m.png`)
             } else if (formIdade < 50) {
                 // adulto
+                img.setAttribute(`src`, `foto-adulto-m.png`)
             } else {
                 // idoso
+                img.setAttribute(`src`, `foto-idosa.png`)
             }
         }
         res.style.textAlign = `center`
-        res.innerHTML = `Detectamos ${gênero} com ${formIdade} anos.`
+        res.innerHTML = `Detectamos ${gênero} com ${formIdade} ano(s).`
+        res.appendChild(img)
     }
 }
