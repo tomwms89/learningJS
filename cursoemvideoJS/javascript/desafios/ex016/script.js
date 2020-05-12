d63476a72c2dc07acf6ffb3d52de23ba4d398472
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-12 09:58:21 -0300

    ok

---
+++ cursoemvideoJS/javascript/desafios/ex016/script.js
@@ -0,0 +1,7 @@
+function contar() {
+    var formInicio = document.querySelector(`div#txtinicio`)
+    var formFinal = document.querySelector(`div#txtfinal`)
+    var res = document.querySelector(`div#res`)
+
+    
+}
