bebffb10a83a9d62c0245b41a8284c7efed27ece
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-22 17:03:15 -0300

    exercício contador

--- cursoemvideoJS/javascript/desafios/ex016/script.js
+++ cursoemvideoJS/javascript/desafios/ex016/script.js
@@ -1,7 +1,34 @@
 function contar() {
-    var formInicio = document.querySelector(`div#txtinicio`)
-    var formFinal = document.querySelector(`div#txtfinal`)
-    var res = document.querySelector(`div#res`)
+    let ini = document.getElementById(`txti`)
+    let fim = document.getElementById(`txtf`)
+    let passo = document.getElementById(`txtp`)
+    let res = document.getElementById(`res`)
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `Impossível contar!`
+        //alert(`[ERRO] Faltam dados!`)
+    } else {
+        res.innerHTML = `Contando: <br>`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert(`Passo inválido! Considerando PASSO 1`)
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
     
 }
